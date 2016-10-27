18b88851a49bd30fbfff1ab751fd70d25f38aab2
Christopher Reitz <christopher@reitz.re>
2016-10-27 14:45:57 +0200

    Be more up-to-date

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Vaporizer",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0),
         .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1),
     ],
     exclude: [
